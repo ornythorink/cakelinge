5e2c3b9b480476d6a78eef5737e1844c26670b96
Ornythorink <orny2007@gmail.com>
2014-11-14 19:09:10 +0100

    iebauche de l'api SDC mais dans le front ça ne va pas, le template appelé par défaut ne va pas (PAGES) changé aussi.

--- cakephp-3.0.0-beta2/lingerie/src/Controller/CategorieController.php
+++ cakephp-3.0.0-beta2/lingerie/src/Controller/CategorieController.php
@@ -14,7 +14,9 @@ class CategorieController extends AppController {
         $this->loadComponent('ImageModifier');
     }
 
-
+    /**
+     * @todo factoriser les appels clients
+     */
     public function index(){
 
         $http = new Client(['host' => 'ornythorink.alwaysdata.net']);
@@ -52,6 +54,7 @@ class CategorieController extends AppController {
         foreach($items as $key => $image){
 
             // FAAAAAKKKKKKEEEE
+            // @todo faire un vrai fake
             $image->imagecache = 'http://localhost/tmp/cache/images/333c84a09b466485b6f9326abb9f687d.jpg';
 
 
@@ -89,10 +92,43 @@ class CategorieController extends AppController {
             }
         }
 
+        //$content = $this->getSDCbyCategory();
+        /* @todo shuffle ?  */
+
         $this->set( 'offres', $items);
 
     }
 
+    /*
+    public function getSDCbyCategory(){
+        $http = new Client(['host' => 'api.ebaycommercenetwork.com']);
+        $response = $http->get('/publisher/3.0/json/GeneralSearch?apiKey=0558e60f-c9fe-4939-a960-a7172cc67783&trackingId=8084776&keyword=culotte&categoryId=96667');
+
+        $content = $response->body();
+        echo '<pre>';
+        $test = json_decode($content);
+        $items = $test->categories->category[0]->items->item;
+
+        // on stocke ou pas .... par catégorie je dirai bien oui, avec cleanage régulier, faudra limiter les appels
+        // et mapper les produits (entities )
+        foreach($items as $key => $value):
+                var_dump($value->offer);
+        endforeach;
+
+        echo '</pre>';
+
+        return $content;
+    }*/
+
+
+
+    /**
+     * @param $url
+     * @param $useCache
+     * @return mixed
+     *
+     * @todo déplacer cette méthode ? refacto ?
+     */
     function resizeImage($url, $useCache){
 
         if($url == ""){
